39361c1739955b6bf5b580ea8ba0f71c03380f92
Adam Wozniak <adamwozhere@gmail.com>
2023-03-17 14:38:40 +0000

    basic CRUD with fieldArray

--- src/pages/new/index.tsx
+++ src/pages/new/index.tsx
@@ -2,7 +2,7 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 
-import { useForm } from 'react-hook-form';
+import { useFieldArray, useForm } from 'react-hook-form';
 import TextField from '@/components/TextField';
 
 import useInvoice from '@/context/InvoiceContext';
@@ -13,13 +13,24 @@ export default function NewInvoice() {
   const {
     handleSubmit,
     register,
+    control,
     formState: { errors },
-  } = useForm<InvoiceSchema>({ resolver: zodResolver(InvoiceValidator) });
+  } = useForm<InvoiceSchema>({
+    resolver: zodResolver(InvoiceValidator),
+    defaultValues: {
+      items: [{ name: '', quantity: 1 }],
+    },
+  });
+
+  const { fields, append, remove } = useFieldArray({
+    name: 'items',
+    control,
+  });
 
   const { dispatch } = useInvoice();
 
   const onSubmit = (data: InvoiceSchema) => {
-    console.log('dispatching CREATE');
+    console.log('dispatching CREATE with data', data);
     // const uid = generateUID();
     dispatch({ type: 'CREATE', data: data });
     router.push('/');
@@ -40,7 +51,22 @@ export default function NewInvoice() {
           fieldProps={register('senderAddress.street')}
           error={errors?.senderAddress?.street?.message}
         />
-
+        <>
+          {fields.map((field, index) => {
+            return (
+              <section key={field.id}>
+                <label htmlFor="">Name</label>
+                <input {...register(`items.${index}.name`)} />
+                <label htmlFor="">Quantity</label>
+                <input {...register(`items.${index}.quantity`)} />
+                <label htmlFor="">Price</label>
+                <input {...register(`items.${index}.price`)} />
+                <label htmlFor="">Total</label>
+                <input {...register(`items.${index}.total`)} />
+              </section>
+            );
+          })}
+        </>
         <button type="submit">Create</button>
       </form>
       <Link href="/">Back</Link>
--- src/schemas/InvoiceSchema.ts
+++ src/schemas/InvoiceSchema.ts
@@ -1,40 +1,55 @@
 import { z } from 'zod';
 import { generateUID } from '@/utils/generateUID';
 
-export const InvoiceValidator = z
-  .object({
-    clientName: z.string().trim().min(1),
-    status: z.enum(['draft', 'pending', 'paid']).default('pending'),
-    senderAddress: z.object({
-      street: z.string().trim().min(1),
-      //city: z.string().min(1),
-    }),
-    items: z.array(
-      z.object({
-        name: z.string(),
-        quantity: z.coerce.number().min(1),
-        price: z.coerce.number(),
-        //total: z.coerce.number(),
-      })
-    ),
-    //total: z.coerce.number(),
-  })
-  .transform((data) => {
-    // set invoice ID
-    let uid: string;
-    if ('id' in data) {
-      uid = data.id as string;
-    } else {
-      uid = generateUID();
-    }
+export const InvoiceValidator = z.object({
+  id: z.string().default(generateUID()),
+  clientName: z.string().trim().min(1),
+  status: z.enum(['draft', 'pending', 'paid']).default('pending'),
+  senderAddress: z.object({
+    street: z.string().trim().min(1),
+    //city: z.string().min(1),
+  }),
+  items: z
+    .array(
+      z
+        .object({
+          name: z.string(),
+          quantity: z.coerce.number().min(1),
+          price: z.coerce.number(),
+          total: z.coerce.number(),
+        })
+        .refine((val) => {
+          const verify = {
+            ...val,
+            total: val.price * val.quantity,
+          };
+          return (
+            val.name === verify.name &&
+            val.quantity === verify.quantity &&
+            val.price === verify.price &&
+            val.total === verify.total
+          );
+        })
+    )
+    .optional(),
+  //total: z.coerce.number(),
+});
+// .transform((data) => {
+//   // set invoice ID
+//   let uid: string;
+//   if ('id' in data) {
+//     uid = data.id as string;
+//   } else {
+//     uid = generateUID();
+//   }
 
-    // set item prices
-    //Object.defineProperty();
+//   // set item prices
+//   //Object.defineProperty();
 
-    return {
-      ...data,
-      id: uid,
-    };
-  });
+//   return {
+//     ...data,
+//     id: uid,
+//   };
+// });
 
 export type InvoiceSchema = z.infer<typeof InvoiceValidator>;
